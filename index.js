81bf2013dd06435d6126112fb4816f44210ad837
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-26 19:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten() {
+ kittens.push("Ralph") 
+ return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten() {
+  return [...kittens, "Broom"]
+}
+
+function prependKitten() {
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten() {
+  kittens.slice(2)
+  return kittens
+}
+
+function removeFirstKitten() {
+  kittens.slice(0)
+  return kittens
+}
